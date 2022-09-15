499547d48d1e2e395815731a817a28c19fc99cf3
Love <loveadeniran1@gmail.com>
2022-09-15 13:01:55 +0100

    wwtbam project in Progress

---
+++ admin/wwtbamAdmin.js
@@ -0,0 +1,59 @@
+// This is the function to add Questions
+
+Question = [];
+const addquestion=()=>{
+    // alert("E dey Work!")
+    let eachQuestion = {
+        question: question.value,
+        option1 : Firstoption.value,
+        option2 : Secondoption.value,
+        option3 : Thirdoption.value,
+        option4 : Fourthoption.value,
+        answer : correctanswer.value
+    }
+    // console.log(eachQuestion)
+    if(question.value==""|| Firstoption.value==""|| Secondoption.value=="" || Thirdoption.value==""|| Fourthoption.value=="" || correctanswer.value==""){
+        Alert("You have to fill in all the fields")
+    }
+    else{
+        Question.push(eachQuestion)
+        console.log(eachQuestion)
+        localStorage.setItem('allQuestion', JSON.stringify(Question))
+        alert("Question Saved Successfully!")
+        question.value = ""
+        Firstoption.value = ""
+        Secondoption.value = ""
+        Thirdoption.value = ""
+        Fourthoption.value = ""
+        correctanswer.value = ""
+        // window.location.href = ""
+        
+    }
+}
+// function addquestion Ends here
+adminDetails = {
+    adminUserName: "Love",
+    adminUserpassword: "love"
+}
+// Admin Login start here
+
+let logIn = document.getElementById('login')
+logIn.addEventListener('click',()=>{
+    let adminName = adminname.value;
+    let adminPassword = adminpassword.value;
+    // console.log(adminName)
+    if(adminname.value==""|| adminpassword.value==""){
+        alert('Fill up the field please!')
+    }
+    else if(adminName== adminDetails.adminUserName && adminPassword== adminDetails.adminUserpassword){
+        alert('login Successful!');
+        // window.location.href = "admin\wwtbamAdmin.html"
+        loginAdmin.style.display= "none"
+    }
+    else{
+       alert('Incorrect name or Password!');
+    }
+})
+
+// Admin Login Ends here
+
--- wwtbam.js
+++ wwtbam.js
@@ -1,31 +1,35 @@
 // let welcome = document.getElementById('Welcome')
+// document.getElementById('test')
 test.style.display = 'none'
 const start =()=>{
     Welcome.style.display ='none'
     test.style.display = 'block'
 }
 
-adminDetails = {
-    adminUserName: "Love",
-    adminUserpassword: "love"
+
+
+const add=()=> {
+    console.log('Okay')
+    window.location.href="/User/addQ.html";
 }
 
 
-let logIn = document.getElementById('login')
-logIn.addEventListener('click',()=>{
-    let adminName = adminname.value;
-    let adminPassword = adminpassword.value;
-    // console.log(adminName)
-    if(adminname.value==""|| adminpassword.value==""){
-        alert('Fill up the field please!')
-    }
-    else if(adminName== adminDetails.adminUserName && adminPassword== adminDetails.adminUserpassword){
-        alert('login Successful!');
-        window.location.href = "/admin/wwtbamAdmin.html"
-    }
-    else{
-       alert('Incorrect name or Password!');
-    }
-})
 
-const add=()=> {window.location.href="addQ.html";}
+// let logIn = document.getElementById('login')
+// logIn.addEventListener('click',()=>{
+//     let adminName = adminname.value;
+//     let adminPassword = adminpassword.value;
+//     // console.log(adminName)
+//     if(adminname.value==""|| adminpassword.value==""){
+//         alert('Fill up the field please!')
+//     }
+//     else if(adminName== adminDetails.adminUserName && adminPassword== adminDetails.adminUserpassword){
+//         alert('login Successful!');
+//         window.location.href = "/admin/wwtbamAdmin.html"
+//     }
+//     else{
+//        alert('Incorrect name or Password!');
+//     }
+// })
+
+
